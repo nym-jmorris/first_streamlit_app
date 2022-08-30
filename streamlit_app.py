b9f57766e4b47acbbd1d39fc28828278a97fb6a5
nym-jmorris <56124050+nym-jmorris@users.noreply.github.com>
2022-08-30 10:06:48 -0400

    Update streamlit_app.py
    Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title("My Parents' New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text('Oatmeal')
+streamlit.text('Berries')
+streamlit.text('Cereal')
+
